0e636e333816d9be8a138eb7a797f6df4443bfbb
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-09-06 12:33:05 -0500

    entidades iniciales
    asdasd

---
+++ app/src/main/java/com/senescyt/app/model/Persona.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senescyt.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author ALEJO PC
+ */
+@Entity
+@Table(name = "Persona")
+public class Persona {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "perId")
+    private Long perId;
+
+    @Getter
+    @Setter
+    @Column(unique = true, name = "perCedula")
+    private String perCedula;
+
+    @Getter
+    @Setter
+    @Column(name = "perNombre")
+    private String perNombre;
+
+    @Getter
+    @Setter
+    @Column(name = "perApellido")
+    private String perApellido;
+
+    @Getter
+    @Setter
+    @Column(name = "perCorreo")
+    private String perCorreo;
+
+    @Getter
+    @Setter
+    @Column(name = "perDireccion")
+    private String perDireccion;
+
+    @Getter
+    @Setter
+    @Column(name = "perTelefono")
+    private String perTelefono;
+
+    @Getter
+    @Setter
+    @Column(name = "perFechaNacimiento")
+    private Date perFechaNacimiento;
+
+    @OneToOne(mappedBy = "usuPerId")
+    private Usuario usuario;
+}
