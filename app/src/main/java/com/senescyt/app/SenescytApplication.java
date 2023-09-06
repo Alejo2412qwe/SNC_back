234b2417d7e62951faab697257f705d42dc4e4b5
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-09-06 11:27:55 -0500

    Add files via upload

---
+++ app/src/main/java/com/senescyt/app/SenescytApplication.java
@@ -0,0 +1,13 @@
+package com.senescyt.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenescytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenescytApplication.class, args);
+	}
+
+}
