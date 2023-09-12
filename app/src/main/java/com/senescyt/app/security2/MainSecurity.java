22f76eeac6f67c287a93b074c54fa13b06ecfe29
BryanCurillo <bryan.curillo.est@tecazuay.edu.ec>
2023-09-12 10:37:01 -0500

    login

--- app/src/main/java/com/senescyt/app/security2/MainSecurity.java
+++ app/src/main/java/com/senescyt/app/security2/MainSecurity.java
@@ -7,17 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MainSecurity extends WebSecurityConfiguration {
-//
-//    @Autowired
-//    UserDetailsServiceImpl userDetailsService;
-//
-//    @Autowired
-//    JwtEntryPoint jwtEntryPoint;
+
 
 }
