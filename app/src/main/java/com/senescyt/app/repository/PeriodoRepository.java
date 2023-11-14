c9bf325d3af3905f383711ba8cdf15bfa0a5e742
Tefi2003 <tefipadillacrespo@hotmail.com>
2023-11-14 08:44:49 -0500

    Periodo

--- app/src/main/java/com/senescyt/app/repository/PeriodoRepository.java
+++ app/src/main/java/com/senescyt/app/repository/PeriodoRepository.java
@@ -3,8 +3,15 @@ package com.senescyt.app.repository;
 import com.senescyt.app.model.Periodo;
 import com.senescyt.app.repository.genericRepository.GenericRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PeriodoRepository extends GenericRepository<Periodo,Long> {
+
+    @Query(value = "SELECT * FROM periodo where peri_estado = :est", nativeQuery = true)
+    public List<Periodo> getPeriodoByEstado(@Param("est") int est);
 }
